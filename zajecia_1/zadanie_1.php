b83d74f557d4943813906a15aafd00732d2ccd85
s30566 <s30566@pjwstk.edu.pl>
2025-03-02 16:42:51 +0100

    zajecia 1 zadanie 1

--- zajecia_1/zadanie_1.php
+++ zajecia_1/zadanie_1.php
@@ -1,2 +1,16 @@
 <?php
-$fruits[]=array("apple", "banana", "orange");
+$fruits=array("apple", "banana", "orange");
+$len=0;
+$rev="";
+foreach($fruits as $fruit){
+    $rev="";
+    while(isset($fruit[$len])){
+        $len++;
+    }
+    for ($i = $len - 1; $i >= 0; $i--) {
+        $rev .= $fruit[$i];
+
+    }
+    $rev .= "\n";
+    echo $rev;
+}

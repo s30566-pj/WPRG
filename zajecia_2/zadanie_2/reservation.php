347fefa23d348f8d7c6acefc84d1f4a19f7ff413
s30566 <s30566@pjwstk.edu.pl>
2025-04-06 19:12:29 +0200

    zad 2 front

--- zajecia_2/zadanie_2/reservation.php
+++ zajecia_2/zadanie_2/reservation.php
@@ -1,1 +1,19 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="utf-8" />
+    <title>Podsumowanie Rezerwacji</title>
+</head>
+<body>
 <?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+$pplCount = isset($_POST['pplCountVal']) ? intval($_POST['pplCountVal']) : -1;
+$fname = isset($_POST['fname']) ? htmlspecialchars($_POST['fname']) : '';
+$lname = isset($_POST['lname']) ? htmlspecialchars($_POST['lname']) : '';
+$email = isset($_POST['email']) ? htmlspecialchars($_POST['email']) : '';
+$phone = isset($_POST['phone']) ? htmlspecialchars($_POST['phone']) : '';
+$childBedSelection = isset($_POST['childBedSelection']) ? intval($_POST['childBedSelection']) : -1;
+}
+?>
+</body>
+</html>

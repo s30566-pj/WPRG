9680c7c09ce58c86330f4054850019bf503b108c
s30566 <s30566@pjwstk.edu.pl>
2025-04-08 17:52:06 +0200

    zad 3 step 2

--- zajecia_2/zadanie_2/step2.php
+++ zajecia_2/zadanie_2/step2.php
@@ -1,19 +1,71 @@
-<!DOCTYPE html>
-<html lang="pl">
-<head>
-    <meta charset="utf-8" />
-    <title>Podsumowanie Rezerwacji</title>
-</head>
-<body>
 <?php
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-$pplCount = isset($_POST['pplCountVal']) ? intval($_POST['pplCountVal']) : -1;
-$fname = isset($_POST['fname']) ? htmlspecialchars($_POST['fname']) : '';
-$lname = isset($_POST['lname']) ? htmlspecialchars($_POST['lname']) : '';
-$email = isset($_POST['email']) ? htmlspecialchars($_POST['email']) : '';
-$phone = isset($_POST['phone']) ? htmlspecialchars($_POST['phone']) : '';
-$childBedSelection = isset($_POST['childBedSelection']) ? intval($_POST['childBedSelection']) : -1;
+session_start();
+
+$pplCount = isset($_POST['pplCountVal']) ? (int) $_POST['pplCountVal'] : -1;
+$_SESSION['pplCount'] = $pplCount;
+$_SESSION['person'][0] = $_POST['person'][0];
+
+if ($pplCount == 1) {
+    header('Location: summary.php');
+    exit;
+} elseif ($pplCount < 1) {
+    headers_send(422);
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Kalkulator</title>
+    <link rel="stylesheet" href="style2.css">
+</head>
+<body>
+<header>
+    <h1>Rezerwacja</h1>
+</header>
+<main>
+    <nav>
+
+    </nav>
+    <article>
+        <form action="summary.php" method="post">
+            <div id="repeatableSection">
+            <?php
+            for ($i = 1; $i < $pplCount; $i++) {
+                echo "
+                <div class = \"basicInfoForm\" >
+                    <div class = \"personalInfo\" >
+                        <h3 > Osoba " . ($i +1) . " </h3 >
+                        <label for=\"fname\" > Imię:</label >
+                        <input type = \"text\" id = \"fname\" name = \"person[".$i."][name]\" placeholder = \"Adam\" required >
+                        <label for=\"lname\" > Nazwisko:</label >
+                        <input type = \"text\" id = \"lname\" name = \"person[".$i."][lastName]\" placeholder = \"Kowalski\" required >
+                        <label for=\"bday\" > Data urodzenia:</label >
+                        <input type = \"date\" id = \"bday\" name = \"person[".$i."][bday]\" required >
+                        <label for=\"email\" > Email:</label >
+                        <input type = \"email\" id = \"email\" name = \"person[".$i."][email]\" placeholder = \"adam@domena.tld\" required >
+                        <label for=\"phone\" > Numer telefonu:</label >
+                        <input type = \"tel\" id = \"phone\" name = \"person[".$i."][phone]\" placeholder = \"+48123456789\" required >
+                    </div >
+                    
+                </div >
+            ";
+            }
+            ?>
+            </div>
+            <div id="buttonSection">
+                <button type="submit">Przejdź dalej</button>
+            </div>
+        </form>
+    </article>
+    <aside>
+
+    </aside>
+</main>
+<footer>
+
+</footer>
+
+
 </body>
 </html>
